679b0483cf6fa59582e848e7abe0ba707e508da1
evgeny.michuk <evgeny.michuk@onefactor.com>
2018-01-23 12:07:56 +0300

    default methods

--- java8-core/src/main/java/com/enmichuk/lambda/LambdaExample.java
+++ java8-core/src/main/java/com/enmichuk/lambda/LambdaExample.java
@@ -3,7 +3,11 @@ package com.enmichuk.lambda;
 public class LambdaExample {
     public static void main(String[] args) {
         MathOperation addition = (int a, int b) -> a + b;
-        System.out.println(addition.operation(1, 3));
+        print(addition);
+    }
+
+    public static void print(MathOperation operation) {
+        System.out.println(operation.operation(1, 3));
     }
 
     interface MathOperation {
