67e44a77b5b233fc9ebbb576213976f27e4c84b9
evgeny.michuk <evgeny.michuk@onefactor.com>
2018-01-23 13:17:31 +0300

    default methods

---
+++ java8-core/src/main/java/com/enmichuk/defaultmethod/DefaultMethodExample.java
@@ -0,0 +1,32 @@
+package com.enmichuk.defaultmethod;
+
+public class DefaultMethodExample {
+    public static void main(String[] args) {
+        Hermaphrodite hermaphrodite = new Hermaphrodite();
+        hermaphrodite.print();
+    }
+}
+
+interface Man {
+    default void print() {
+        System.out.println("I'm a man");
+    }
+    static void anotherPrint() {
+        System.out.println("Also I can do this");
+    }
+}
+
+interface Woman {
+    default void print() {
+        System.out.println("I'm a woman");
+    }
+}
+
+class Hermaphrodite implements Man, Woman {
+    public void print() {
+        Man.super.print();
+        Woman.super.print();
+        System.out.println("Because I'm a hermaphrodite");
+        Man.anotherPrint();
+    }
+}
