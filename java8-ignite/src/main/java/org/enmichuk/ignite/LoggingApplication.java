d42cbb58227bc092709a81433c649fc888b156f1
evgeny.michuk <evgeny.michuk@onefactor.com>
2018-02-07 17:02:06 +0300

    first ignite samples

---
+++ java8-ignite/src/main/java/org/enmichuk/ignite/LoggingApplication.java
@@ -0,0 +1,22 @@
+package org.enmichuk.ignite;
+
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteLogger;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.logger.slf4j.Slf4jLogger;
+
+public class LoggingApplication {
+    public static void main(String[] args) {
+        IgniteConfiguration cfg = new IgniteConfiguration();
+
+        IgniteLogger log = new Slf4jLogger();
+
+        cfg.setGridLogger(log);
+
+        // Start Ignite node.
+        Ignite ignite = Ignition.start(cfg);
+
+        ignite.log().info("Info Message Logged!");
+    }
+}
