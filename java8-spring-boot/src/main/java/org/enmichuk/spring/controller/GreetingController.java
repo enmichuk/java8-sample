07a2dd4cb7983d6e75a614f6275775297eb43c29
evgeny.michuk <evgeny.michuk@onefactor.com>
2018-02-20 13:41:18 +0300

    sample spring RESTful app

---
+++ java8-spring-boot/src/main/java/org/enmichuk/spring/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package org.enmichuk.spring.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.enmichuk.spring.model.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController//same as @Controller and @ResponseBody rolled together
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
