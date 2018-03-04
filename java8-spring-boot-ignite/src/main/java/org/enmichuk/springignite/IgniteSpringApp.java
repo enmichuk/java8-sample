262c7ac2c30b2598c1d97623d7bf856252041c81
Evgeniy Michuk <enmichuk@mail.ru>
2018-03-05 00:07:33 +0300

    added useful links

--- java8-spring-boot-ignite/src/main/java/org/enmichuk/springignite/IgniteSpringApp.java
+++ java8-spring-boot-ignite/src/main/java/org/enmichuk/springignite/IgniteSpringApp.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IgniteSpringApp {
+    //https://spring.io/guides/gs/accessing-data-jpa/
+    //https://dzone.com/articles/spring-boot-integration-with-apache-ignite-and-its
+    //https://apacheignite-mix.readme.io/v2.3/docs/spring-data
+    //https://apacheignite-mix.readme.io/v2.3/docs/spring-caching
     public static void main(String[] args) {
         SpringApplication.run(IgniteSpringApp.class, args);
     }
